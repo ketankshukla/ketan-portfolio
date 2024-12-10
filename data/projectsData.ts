272e3b8e56dedcc069a17a18ad559d66dfc54fdc
Ketan Shukla <ketankshukla@gmail.com>
2024-12-09 18:47:53 -0800

    updated portfolio link in projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,7 +9,7 @@ const projectsData: Project[] = [
   {
     title: 'Portfolio Website',
     description: `My personal portfolio website built with Next.js, Tailwind CSS, and TypeScript. Features a blog section with MDX support and GitHub Discussions integration for comments.`,
-    href: 'https://github.com/ketankshukla/portfolio-new',
+    href: 'https://github.com/ketankshukla/ketan-portfolio',
   },
 ]
 
